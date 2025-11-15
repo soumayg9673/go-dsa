61a5e11f885195b6089f83dd41561effd6125ca1
soumayg9673 <soumayg9673@zohomail.in>
2025-11-15 23:53:33 +0530

    array: majority element optimal solution

--- arrays/14_majority_element/main.go
+++ arrays/14_majority_element/main.go
@@ -27,6 +27,44 @@ func naiveApproach(arr []int) []int {
 	return l
 }
 
+/*
+Time Complexity: O(n)
+Auxilary Space: O(1)
+*/
+func optimalSolution(arr []int) []int {
+	res := 0
+	count := 1
+
+	for i := 1; i < len(arr); i++ {
+		if arr[res] == arr[i] {
+			count++
+		} else {
+			count = 0
+		}
+
+		if count == 0 {
+			res = i
+			count = 1
+		}
+	}
+
+	l := []int{}
+	count = 0
+
+	for i := range arr {
+		if arr[i] == arr[res] {
+			count++
+			l = append(l, i)
+		}
+	}
+
+	if count > len(arr)/2 {
+		return l
+	} else {
+		return []int{}
+	}
+}
+
 func main() {
 	data := [][]int{
 		{8, 3, 4, 8, 8},              // 0 or 3 or 4
@@ -37,5 +75,10 @@ func main() {
 	fmt.Println("Naive approach")
 	for _, d := range data {
 		fmt.Println(naiveApproach(d))
+	}
+
+	fmt.Println("Optimal approach")
+	for _, d := range data {
+		fmt.Println(optimalSolution(d))
 	}
 }
