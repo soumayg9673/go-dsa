5b98d90b4b9d18b0493e8e4bc19cb5b54bb9983d
soumayg9673 <soumayg9673@zohomail.in>
2025-11-16 04:13:45 +0530

    array: prefix sum using optimal solution

--- arrays/17_prefix_sum/main.go
+++ arrays/17_prefix_sum/main.go
@@ -3,10 +3,10 @@ package main
 import "fmt"
 
 type Data struct {
-	list  []int
-	query []struct {
-		start int
-		end   int
+	List  []int
+	Query []struct {
+		Start int
+		End   int
 	}
 }
 
@@ -15,33 +15,64 @@ Time Complexity: O(nm)
 Auxilary Space: O(1)
 */
 func (d Data) naiveApproach() []int {
-	list := make([]int, 0, len(d.query))
-	for _, q := range d.query {
+	list := make([]int, 0, len(d.Query))
+	for _, q := range d.Query {
 		sum := 0
-		for i := q.start; i <= q.end; i++ {
-			sum += d.list[i]
+		for i := q.Start; i <= q.End; i++ {
+			sum += d.List[i]
 		}
 		list = append(list, sum)
 	}
 	return list
 }
 
+/*
+Time Complexity: O(n)
+Auxilary Space: O(n)
+*/
+func (d Data) optimalSolution() []int {
+
+	// Calculate cummulative sum from start
+	leftSum := make([]int, len(d.List))
+	leftSum[0] = d.List[0]
+	for i := 1; i < len(d.List); i++ {
+		leftSum[i] = leftSum[i-1] + d.List[i]
+	}
+
+	list := make([]int, 0, len(d.Query))
+	for _, q := range d.Query {
+		if q.Start == 0 {
+			list = append(list, leftSum[q.End])
+		} else {
+			list = append(list, leftSum[q.End]-leftSum[q.Start-1])
+		}
+	}
+
+	return list
+}
+
 func main() {
 	data := []Data{
 		{
-			list: []int{2, 8, 3, 9, 6, 5, 4},
-			query: []struct {
-				start int
-				end   int
+			List: []int{2, 8, 3, 9, 6, 5, 4},
+			Query: []struct {
+				Start int
+				End   int
 			}{
-				{start: 0, end: 2},
-				{start: 1, end: 3},
-				{start: 2, end: 6},
+				{Start: 0, End: 2},
+				{Start: 1, End: 3},
+				{Start: 2, End: 6},
 			},
 		},
 	}
 
+	fmt.Println("Naive approach")
 	for _, d := range data {
 		fmt.Println(d.naiveApproach())
+	}
+
+	fmt.Println("Optimal approach")
+	for _, d := range data {
+		fmt.Println(d.optimalSolution())
 	}
 }
