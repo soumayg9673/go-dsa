d6b3f1f4f13e55d8d834b91f3fbf16e01718a803
soumayg9673 <soumayg9673@zohomail.in>
2025-11-15 21:36:09 +0530

    array: longest odd even subarray

---
+++ arrays/longest_odd_even_subarray/main.go
@@ -0,0 +1,37 @@
+package main
+
+import "fmt"
+
+/*
+Algorith: Kadane's algorithm
+=	Time Complexity: O(n)
+-	Auxilary Space: O(1)
+*/
+func optimalApproach(arr []int) int {
+	res, temp := 1, 1
+
+	for i := 1; i < len(arr); i++ {
+		if arr[i-1]%2 != arr[i]%2 {
+			temp++
+			res = max(res, temp)
+		} else {
+			temp = 1
+		}
+	}
+
+	return res
+}
+
+func main() {
+	data := [][]int{
+		{10, 12, 14, 7, 8}, // 3
+		{7, 10, 13, 14},    // 4
+		{10, 12, 8, 4},     // 1
+	}
+
+	fmt.Println("Naive approach")
+	for _, d := range data {
+		fmt.Println(optimalApproach(d))
+	}
+
+}
