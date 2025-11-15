e1bb55ba99f2046f821d287471366b0c9627c288
soumayg9673 <soumayg9673@zohomail.in>
2025-11-15 20:16:40 +0530

    array; max subarray sum naive approach

---
+++ arrays/max_subarray_sum/main.go
@@ -0,0 +1,29 @@
+package main
+
+import "fmt"
+
+func naiveApproach(arr []int) int {
+	res := arr[0]
+
+	for i := range arr {
+		temp := 0
+		for j := i; j < len(arr); j++ {
+			temp += arr[j]
+		}
+		res = max(res, temp, arr[i])
+	}
+	return res
+}
+
+func main() {
+	data := [][]int{
+		{2, 3, -8, 7, -1, 2, 3},
+		{5, 8, 3},
+		{-6, -1, -7},
+	}
+
+	fmt.Println("Naive approach")
+	for _, d := range data {
+		fmt.Println(naiveApproach(d))
+	}
+}
