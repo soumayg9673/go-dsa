7795c23f6419207eef4c96a88594af3699dc1a72
soumayg9673 <soumayg9673@zohomail.in>
2025-11-16 02:26:44 +0530

    array: sliding window sum with optimal solution

--- arrays/15_sliding_window_tech/main.go
+++ arrays/15_sliding_window_tech/main.go
@@ -1,18 +1,43 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"math"
+)
 
 /*
-Time Complexity: O(n^2)
+Time Complexity: O(nk)
 Auxilary Space: O(1)
 */
 func naiveApproach(arr []int, k int) int {
-	res := arr[k-1]
+	res := math.MinInt
 
 	for i := 0; i < len(arr) && i+k-1 < len(arr); i++ {
 		temp := 0
-		for j := 0; j < k; j++ {
+		for j := range k {
 			temp += arr[i+j]
+		}
+		res = max(res, temp)
+	}
+
+	return res
+}
+
+/*
+Time Complexity: O(n)
+Auxilary Space: O(1)
+*/
+func optimalSolution(arr []int, k int) int {
+	res := math.MinInt
+	temp := 0
+
+	for i := 0; i+k-1 < len(arr); i++ {
+		if i == 0 {
+			for j := range k {
+				temp += arr[j]
+			}
+		} else {
+			temp = temp - arr[i-1] + arr[i+k-1]
 		}
 		res = max(res, temp)
 	}
@@ -38,5 +63,10 @@ func main() {
 	fmt.Println("Naive approach")
 	for _, d := range data {
 		fmt.Println(naiveApproach(d.l, d.k))
+	}
+
+	fmt.Println("Optimal approach")
+	for _, d := range data {
+		fmt.Println(optimalSolution(d.l, d.k))
 	}
 }
